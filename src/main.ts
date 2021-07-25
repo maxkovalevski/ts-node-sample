76e18400867320b30099e497096bb93d397340a9
kowalevski <me@kowalevski.com>
2021-07-25 20:44:28 +0200

    created an example

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { createQuestioner } from "./createQuestioner";
 import { greeting } from "./greeting";
 
+import inventory from "./inventory.json";
+
 async function main() {
   try {
     const questioner = createQuestioner();
-    const firstName = await questioner.ask("Type your first name: ");
     const username = await questioner.ask("Type your username: ");
 
-    greeting(firstName, username);
+    greeting(username);
+
+    const itemName = await questioner.ask(
+      "Type the name of the inventory item: "
+    );
+
+    const foundItem = inventory.find((item) => item.name === itemName);
+
+    console.log(
+      `You've chosen an item: ${foundItem.icon} ${foundItem.name} (lvl ${foundItem.level})`
+    );
 
     questioner.finishUp();
   } catch (e) {
